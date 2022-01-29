eeb5d5eb41607736ace181b0c7c38f5056df137b
Patrick Moffett <37123538+Patrick-Moffett@users.noreply.github.com>
2022-01-29 13:37:51 -0800

    Added TextMeshPro, and MainMenu with Background Music Singleton Script

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic _instance;
+    public static BackgroundMusic Instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
