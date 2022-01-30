ba1a4cd4e77cbf28b2f71e8f1624e3101083ca69
Patrick Moffett <37123538+Patrick-Moffett@users.noreply.github.com>
2022-01-30 12:09:02 -0800

    Added Spike SFX to SpikeTilemaps, and script

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
+    AudioSource m_AudioSource;
+    private void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+        if(m_AudioSource == null)
+        {
+            Debug.LogError("Audio source not set on spikes");
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<PlayerScript>().Die();
+            m_AudioSource.Play();
         }
     }
 }
